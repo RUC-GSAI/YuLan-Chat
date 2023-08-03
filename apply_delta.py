bc0bbf7edde18cc6f1baee1e87ede847f032d3ff
FrancisZhou <1046946079@qq.com>
2023-08-03 14:48:00 +0800

    Update bf16

--- apply_delta.py
+++ apply_delta.py
@@ -11,13 +11,13 @@ def apply_diff(path_raw, path_tuned, path_diff, device="cpu"):
     model_diff = AutoModelForCausalLM.from_pretrained(
         path_diff,
         device_map={"": torch.device(device)},
-        torch_dtype=torch.float32,
+        torch_dtype=torch.bfloat16,
         low_cpu_mem_usage=True,
     )
     model_raw = AutoModelForCausalLM.from_pretrained(
         path_raw,
         device_map={"": torch.device(device)},
-        torch_dtype=torch.float32,
+        torch_dtype=torch.bfloat16,
         low_cpu_mem_usage=True,
     )
 
@@ -29,7 +29,7 @@ def apply_diff(path_raw, path_tuned, path_diff, device="cpu"):
     for key in tqdm.tqdm(state_dict_diff):
         if (state_dict_raw[key].size() != state_dict_diff[key].size()):
             delta = state_dict_diff[key].size(0) - state_dict_raw[key].size(0)
-            state_dict_raw[key] = torch.cat((state_dict_raw[key], torch.zeros((delta, state_dict_raw[key].size(1)), dtype=torch.float32)))
+            state_dict_raw[key] = torch.cat((state_dict_raw[key], torch.zeros((delta, state_dict_raw[key].size(1)), dtype=torch.bfloat16)))
             print(key)
             print(state_dict_raw[key].size(), state_dict_diff[key].size())
         state_dict_diff[key].add_(state_dict_raw[key])
